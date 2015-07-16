ce125c8d1f260bc3b4b34a56106f06d41137741e
Enzo Hamelin <enzo.hamelin@gmail.com>
2015-07-16 11:01:35 +0200

    Compatibilité avec systempay

--- confirmbasketnotempty.js
+++ confirmbasketnotempty.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
     var link_intern = false;
     document.addEventListener("click", function(e) {
-	if (e.currentTarget.activeElement.nodeName.toLowerCase() === 'a'  && document.location.host === e.currentTarget.activeElement.host || e.target.href === 'javascript:void(0)' || e.currentTarget.activeElement.nodeName.toLowerCase() === 'button' || e.currentTarget.activeElement.nodeName.toLowerCase() === 'input' || e.currentTarget.activeElement.nodeName.toLowerCase() === 'body') {
+	if (e.currentTarget.activeElement.nodeName.toLowerCase() === 'a'  && document.location.host === e.currentTarget.activeElement.host || e.target.href.replace(/[\s\;]/g,"") === 'javascript:void(0)' || e.currentTarget.activeElement.nodeName.toLowerCase() === 'button' || e.currentTarget.activeElement.nodeName.toLowerCase() === 'input' || e.currentTarget.activeElement.nodeName.toLowerCase() === 'body') {
 	    link_intern = true;
 	}
     }, true);
     $(window).bind('beforeunload', function(e) {
 	if(link_intern)
+	    return;
+	if(e.srcElement.activeElement.className.indexOf("redirect") > -1)
 	    return;
 	if(parseInt($('.ajax_cart_quantity').text(), 10) == 0)
 	    return;
