caf72fce4ad4cd3cf77e2ea07b4e6d2a6a698cbf
Enzo Hamelin <enzo.hamelin@gmail.com>
2015-07-17 14:19:44 +0200

    Traduction française

---
+++ translations/fr.php
@@ -0,0 +1,14 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{confirmbasketnotempty}prestashop>confirmbasketnotempty_be34130166cc8b8f13a4785e9ac970d0'] = 'Confirmation panier non vide';
+$_MODULE['<{confirmbasketnotempty}prestashop>confirmbasketnotempty_72931d732708ff9a90ddf99bf278e4b8'] = 'Module permettant d\'afficher un message quand l\'utilisateur quitte le site alors que son panier n\'est pas vide.';
+$_MODULE['<{confirmbasketnotempty}prestashop>confirmbasketnotempty_d6b901f3712b0b3805f0c618018e10ce'] = 'Etes vous sur de vouloir désinstaller Confirmation panier non vide ?';
+$_MODULE['<{confirmbasketnotempty}prestashop>confirmbasketnotempty_5624beb288545f9c9dcc9a515d04422b'] = 'Aucun message enregistré';
+$_MODULE['<{confirmbasketnotempty}prestashop>confirmbasketnotempty_fe5d926454b6a8144efce13a44d019ba'] = 'La configuration est invalide';
+$_MODULE['<{confirmbasketnotempty}prestashop>confirmbasketnotempty_c888438d14855d7d96a2724ee9c306bd'] = 'Réglages mis à jour';
+$_MODULE['<{confirmbasketnotempty}prestashop>confirmbasketnotempty_f4f70727dc34561dfde1a3c529b6205c'] = 'Réglages';
+$_MODULE['<{confirmbasketnotempty}prestashop>confirmbasketnotempty_95046bf3c8f7d8a1b421b43efbc530d5'] = 'Valeur du message';
+$_MODULE['<{confirmbasketnotempty}prestashop>confirmbasketnotempty_c9cc8cce247e49bae79f15173ce97354'] = 'Enregistrer';
+$_MODULE['<{confirmbasketnotempty}prestashop>confirmbasketnotempty_630f6dc397fe74e52d5189e2c80f282b'] = 'Retourner à la liste';
